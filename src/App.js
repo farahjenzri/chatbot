f01c050a1cd562e7117760e159bec74a5ed0fa8f
Farahjenzri <farrah9120@gmail.com>
2022-04-22 01:06:44 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Chat from './component/Chat';
+function App() {
+  return (
+    <div className="App">
+      <div>start a conversation</div>
+      <Chat/>
+    </div>
+  );
+}
+
+export default App;
